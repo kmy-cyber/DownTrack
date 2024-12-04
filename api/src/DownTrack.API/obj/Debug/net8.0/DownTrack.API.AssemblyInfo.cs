7f6ab1afac674df5a87391c9469068ad62d30ca7
JabelResendiz <jabelresendiz18@gmail.com>
2024-12-03 20:21:07 -0500

    clean_architecture

--- api/src/DownTrack.API/obj/Debug/net8.0/DownTrack.API.AssemblyInfo.cs
+++ api/src/DownTrack.API/obj/Debug/net8.0/DownTrack.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DownTrack.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a242de1a5b5ecc3af55b990dfb532f5da6707bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c98c097732cfb0d1265c1f84827ee98ebd4413e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DownTrack.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DownTrack.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
