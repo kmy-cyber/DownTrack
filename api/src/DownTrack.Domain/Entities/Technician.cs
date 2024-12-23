3cdbd0666acfc70c97e3c5425a70705e142f81c4
JabelResendiz <jabelresendiz18@gmail.com>
2024-12-23 16:35:05 -0500

    configuring the API

---
+++ api/src/DownTrack.Domain/Entities/Technician.cs
@@ -0,0 +1,10 @@
+
+
+namespace DownTrack.Domain.Enitites;
+
+public class Technician
+{
+    public string Name{get;set;}
+    public string Specialty{get;set;}
+    public double Salary{get;set;}
+}
