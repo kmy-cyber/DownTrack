37296e6de567b82e98a046f483208711b2c1ea29
noelpc03 <noel2692003@gmail.com>
2024-12-27 22:23:06 -0500

    added equipment entity

---
+++ api/src/DownTrack.Domain/Entities/Equipment.cs
@@ -0,0 +1,12 @@
+using DownTrack.Domain.Enitites;
+
+namespace DownTrack.Domain.Entities
+{
+    public class Equipment : GenericEntity
+    {
+        public string Name { get; set; } = "name";
+        public string Type { get; set; } = "type";
+        public string Status { get; set; } = "status";
+        public DateTime DateOfadquisition { get; set; } = DateTime.Now;
+    }
+}
