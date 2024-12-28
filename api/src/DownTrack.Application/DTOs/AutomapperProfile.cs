44d1abf5fa12de6d2ddc4760e6dea7753d1d2a38
noelpc03 <noel2692003@gmail.com>
2024-12-28 14:25:40 -0500

    modified AutomapperProfile

--- api/src/DownTrack.Application/DTOs/AutomapperProfile.cs
+++ api/src/DownTrack.Application/DTOs/AutomapperProfile.cs
@@ -3,6 +3,7 @@
 
 using AutoMapper;
 using DownTrack.Domain.Enitites;
+using DownTrack.Domain.Entities;
 
 namespace DownTrack.Application.DTO;
 
@@ -12,5 +13,7 @@ public class AutomapperProfile : Profile
     {
         CreateMap<TechnicianDto, Technician>();
         CreateMap<Technician,TechnicianDto>();
+        CreateMap<EquipmentDto, Equipment>();
+        CreateMap<Equipment, EquipmentDto>();
     }
 }
