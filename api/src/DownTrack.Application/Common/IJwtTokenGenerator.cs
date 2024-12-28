7ca839dd8ee0deb35e45aab4d828ef39be631083
JabelResendiz <jabelresendiz18@gmail.com>
2024-12-28 14:28:16 -0500

    generate jwt token

--- api/src/DownTrack.Application/Common/IJwtTokenGenerator.cs
+++ api/src/DownTrack.Application/Common/IJwtTokenGenerator.cs
@@ -6,5 +6,10 @@ namespace DownTrack.Application.Common.Authentication;
 
 public interface IJwtTokenGenerator
 {
-    string GenerateToken(User userf);
+    /// <summary>
+    /// Generates a JWT token for the specified user.
+    /// </summary>
+    /// <param name="user">The user for whom the token is being generated.</param>
+    /// <returns>A JWT token as a string.</returns>
+    string GenerateToken(User user);
 }
---
+++ api/src/DownTrack.Infrastructure/Authentication/JwtSettings.cs
@@ -0,0 +1,16 @@
+
+
+namespace DownTrack.Infrastructure.Authentication;
+
+/// <summary>
+/// Container for storing JWT (JSON Web Token) related configurations, which are used in user authentication and authorization.
+/// </summary> 
+public class JwtSettings
+{
+    public const string SECTION_NAME = "JwtSettings";
+
+    public string Secret { get; init; } = null!;
+    public string Issuer { get; init; } = null!;
+    public int ExpiryMinutes { get; init; }
+    public string Audience { get; init; } = null!;
+}
