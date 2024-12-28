c51c9b7d588e562e252ececf677fe126be9ab9ff
noelpc03 <noel2692003@gmail.com>
2024-12-27 22:43:15 -0500

    added equipment dto

--- api/src/DownTrack.Application/DTOs/TechnicianDto.cs
+++ api/src/DownTrack.Application/DTOs/TechnicianDto.cs
@@ -4,9 +4,9 @@ namespace DownTrack.Application.DTO;
 
 public class TechnicianDto
 {
-    public int Id {get;set;}
+    public int Id { get; set; }
     public string Name { get; set; } = "name";
-    public string Specialty { get; set; }="specialty";
+    public string Specialty { get; set; } = "specialty";
     public double Salary { get; set; }
     public int ExpYears { get; set; }
 
