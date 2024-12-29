31617cd49c8c5cac793ff14741aa799966f7a715
amabe418 <valienteamalia418@gmail.com>
2024-12-28 23:42:31 -0500

    Application layer implemented

---
+++ api/src/DownTrack.Application/DTOs/SectionDto.cs
@@ -0,0 +1,10 @@
+namespace DownTrack.Application.DTO
+{
+    public class SectionDto
+    { 
+        public int Id {get;set;}
+
+         public required string Name { get; set; } = "name";
+    }
+
+}
