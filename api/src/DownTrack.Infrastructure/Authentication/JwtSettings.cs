8d4f26f8de5f91176c3f8f8b31ed2dfe2bf9beb4
JabelResendiz <jabelresendiz18@gmail.com>
2024-12-31 01:52:23 -0500

    added JwtTokenGenerator

--- api/src/DownTrack.Infrastructure/Authentication/JwtSettings.cs
+++ api/src/DownTrack.Infrastructure/Authentication/JwtSettings.cs
@@ -8,7 +8,6 @@ namespace DownTrack.Infrastructure.Authentication;
 public class JwtSettings
 {
     public const string SECTION_NAME = "JwtSettings";
-
     public string Secret { get; init; } = null!;
     public string Issuer { get; init; } = null!;
     public int ExpiryMinutes { get; init; }
