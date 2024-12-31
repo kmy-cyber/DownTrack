6f7dcbda1833effa398d0f23368fb00bbfb2efac
noelpc03 <noel2692003@gmail.com>
2024-12-30 19:50:56 -0500

    Update EquipmentDto to use EquipmentStatus enum and add JsonIgnore for EquipmentDecommissioning

--- api/src/DownTrack.Application/DTOs/EquipmentDto.cs
+++ api/src/DownTrack.Application/DTOs/EquipmentDto.cs
@@ -1,3 +1,7 @@
+using System.Text.Json.Serialization;
+using DownTrack.Domain.Entities;
+using DownTrack.Domain.Enum;
+
 namespace DownTrack.Application.DTO
 {
     public class EquipmentDto
@@ -5,8 +9,11 @@ namespace DownTrack.Application.DTO
         public int Id {get;set;}
         public string Name { get; set; } = "name";
         public string Type { get; set; } = "type";
-        public string Status { get; set; } = "status";
+        public EquipmentStatus Status { get; set; }
         public DateTime DateOfadquisition { get; set; } = DateTime.Now;
+        
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public EquipmentDecommissioning? EquipmentDecommissioning { get; set; } = null;
 
     }
 }
