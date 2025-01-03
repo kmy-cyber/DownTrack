ab986e985e8f4e1e12ebd37688085291ce0e03df
Kmy <ariannepalancar0310@gmail.com>
2025-01-03 06:59:12 -0500

    adding info-prototype

--- frontend/src/pages/dashboard_receptor/info_disposal.jsx
+++ frontend/src/pages/dashboard_receptor/info_disposal.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { CardBody, Typography, Button } from "@material-tailwind/react";
-
+import {InformationCircleIcon} from "@heroicons/react/24/outline";
 const DisposalInfoForm = ({ disposal, onClose  }) => {
     
     return (
-        <div className="p-6">
+        <div className="p-6 ">
             <CardBody>
-                <Typography variant="h5" className="mb-4">
+                <div className="flex items-center justify-center">
+                    <InformationCircleIcon className="h-20 w-120 text-blue-800" />
+                </div>
+                <Typography variant="h5" className="mb-4 flex items-center justify-center">
                     Low-Technical Suggestion Information
                 </Typography>
                 <div className="space-y-4">
