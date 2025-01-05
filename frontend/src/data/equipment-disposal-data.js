2a09e330339c4f6b187389eadeb128e464f8e5d7
jotica0602 <alejandroeb5271@gmail.com>
2025-01-04 23:43:23 -0500

    added data for testing

--- frontend/src/data/equipment-disposal-data.js
+++ frontend/src/data/equipment-disposal-data.js
@@ -61,6 +61,83 @@ export const equipmentDisposalData = [
         equipment: "Keyboard",
         reasonForRemoval: "Stuck keys",
         date: "2023-05-23"
+    },
+    {
+        id: 10,
+        technician: "Carlos Ramirez",
+        equipment: "Router",
+        reasonForRemoval: "Connection issues",
+        date: "2023-06-01"
+    },
+    {
+        id: 11,
+        technician: "Sophia Williams",
+        equipment: "Projector",
+        reasonForRemoval: "Overheating",
+        date: "2023-06-02"
+    },
+    {
+        id: 12,
+        technician: "David Clark",
+        equipment: "Speaker",
+        reasonForRemoval: "Audio distortion",
+        date: "2023-06-03"
+    },
+    {
+        id: 13,
+        technician: "Rachel Lee",
+        equipment: "Camera",
+        reasonForRemoval: "Lens malfunction",
+        date: "2023-06-04"
+    },
+    {
+        id: 14,
+        technician: "Daniel Scott",
+        equipment: "Scanner",
+        reasonForRemoval: "Broken glass",
+        date: "2023-06-05"
+    },
+    {
+        id: 15,
+        technician: "Olivia Davis",
+        equipment: "Smartwatch",
+        reasonForRemoval: "Battery drain",
+        date: "2023-06-06"
+    },
+    {
+        id: 16,
+        technician: "James Wilson",
+        equipment: "Air conditioner",
+        reasonForRemoval: "Cooling failure",
+        date: "2023-06-07"
+    },
+    {
+        id: 17,
+        technician: "Chloe Martinez",
+        equipment: "GPS Device",
+        reasonForRemoval: "Unresponsive screen",
+        date: "2023-06-08"
+    },
+    {
+        id: 18,
+        technician: "Ethan Moore",
+        equipment: "External hard drive",
+        reasonForRemoval: "File corruption",
+        date: "2023-06-09"
+    },
+    {
+        id: 19,
+        technician: "Liam Taylor",
+        equipment: "WIFI Extender",
+        reasonForRemoval: "Signal loss",
+        date: "2023-06-10"
+    },
+    {
+        id: 20,
+        technician: "Mason Harris",
+        equipment: "Printer",
+        reasonForRemoval: "Paper jam issues",
+        date: "2023-06-11"
     }
 ];
 
