1f5589fe49d3e485631e5a45e774ff36ce51d841
amabe418 <valienteamalia418@gmail.com>
2025-01-16 11:07:05 -0500

    Transfer Entity created

---
+++ api/src/DownTrack.Domain/Entities/Transfer.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DownTrack.Domain.Entities;
+
+public class Transfer : GenericEntity
+{
+    public int? RequestId { get; set; }
+    public TransferRequest? TransferRequest{get; set;}
+
+    public int ShippingSupervisorId {get; set;}
+    public Employee? ShippingSupervisor {get; set;}
+
+    public int EquipmentReceptorId {get; set;}
+    public Employee? EquipmentReceptor{get; set;}
+    public DateTime Date { get; set; } 
+}
