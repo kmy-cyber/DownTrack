a10c2f571f17977a294e99bca19ad36fea23f865
JabelResendiz <jabelresendiz18@gmail.com>
2025-01-20 14:39:51 -0500

    added paginationResult

--- api/tests/ManualTests/Program.cs
+++ api/tests/ManualTests/Program.cs
@@ -1,9 +1,38 @@
-﻿using Manual;
+﻿// using Manual;
 
-var apiClient = new ApiClient();
+// var apiClient = new ApiClient();
 
-// await apiClient.CreateEquipmentAsync();
+// // await apiClient.CreateEquipmentAsync();
 
-await apiClient.RegisterUserAsync();
+// await apiClient.RegisterUserAsync();
 
-// await apiClient.DeleteEquipmentAsync();
+// // await apiClient.DeleteEquipmentAsync();
+
+public System.Linq.Expression;
+
+
+// Agregar algunos valores a la lista
+Lista.list.AddRange(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+
+// Consultar números mayores a 5
+var result = Lista.Query(x => x > 5);
+
+// Mostrar los resultados
+Console.WriteLine("Números mayores que 5:");
+foreach (var num in result)
+{
+    Console.WriteLine(num);
+}
+
+public static class Lista
+{
+    public static List<int> list = new List<int>();
+
+    public static IQueryable<int> Query(Expression<Func<int, bool>> exp)
+    {
+
+        return list.AsQueryable().Where(exp);
+
+    }
+
+}
