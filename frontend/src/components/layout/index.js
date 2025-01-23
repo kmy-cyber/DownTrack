5f7a21fb765d25fc43eeec143ecde0c5c786973c
JabelResendiz <jabelresendiz18@gmail.com>
2025-01-22 22:10:19 -0500

    deleted frontend

--- frontend/src/components/layout/index.js
+++
@@ -1,6 +0,0 @@
-export * from "@/components/layout/sidenav";
-export * from "@/components/layout/dashboard-navbar";
-export * from "@/components/layout/configurator";
-export * from "@/components/layout/footer";
-export * from "@/components/layout/navbar";
-export * from "@/components/layout/profile_small_info";
