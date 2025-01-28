b004813ad658b6d6eb455bbdaf25a658a1fbbe9e
JabelResendiz <jabelresendiz18@gmail.com>
2025-01-27 23:00:43 -0500

    Removed frontend/ folder from api branch

--- frontend/src/components/sections/index.js
+++
@@ -1,2 +0,0 @@
-export * from "@/components/sections/sections_table"
-export * from "@/components/sections/section_details"
--- frontend/src/pages/dashboard_technic/index.js
+++
@@ -1,4 +0,0 @@
-export * from "@/pages/dashboard_technic/home";
-export * from "@/pages/dashboard_technic/insert_maintenance";
-export * from "@/pages/dashboard_technic/insert_technical_leave";
-export * from "@/pages/dashboard_technic/equipment_inventory";
