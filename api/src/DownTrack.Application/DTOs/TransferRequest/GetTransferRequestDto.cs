bcff633b4896707f6ce22e188fb441fda3afa2b8
amabe418 <valienteamalia418@gmail.com>
2025-01-28 16:40:54 -0500

    equipment decomissioning by receptor id implemented

--- api/src/DownTrack.Application/DTOs/TransferRequest/GetTransferRequestDto.cs
+++ api/src/DownTrack.Application/DTOs/TransferRequest/GetTransferRequestDto.cs
@@ -7,13 +7,13 @@ public class GetTransferRequestDto
     public int SectionManagerId { get; set; }
     public string SectionManagerUserName {get;set;}= null!;
     public int EquipmentId { get; set; }
-    public string EquipmentName {get; set;}
-    public string EquipmentStatus {get; set;}
-    public string EquipmentType {get; set;}
-    public string RequestDepartmentId {get; set;}
-    public string RequestDepartmentName{get; set;}
-    public string RequestSectionId {get; set;}
-    public string RequestSectionName {get; set;}
+    public string? EquipmentName {get; set;}
+    public string? EquipmentStatus {get; set;}
+    public string? EquipmentType {get; set;}
+    public string? RequestDepartmentId {get; set;}
+    public string? RequestDepartmentName{get; set;}
+    public string? RequestSectionId {get; set;}
+    public string? RequestSectionName {get; set;}
     public int ArrivalDepartmentId { get; set; }
     public int ArrivalSectionId {get;set;}
     public string ArrivalDepartmentName {get;set;} =null!;
