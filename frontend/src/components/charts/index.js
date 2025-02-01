1edae70b9cb970409568d0abcb1d7ac289acea83
Kmy <ariannepalancar0310@gmail.com>
2025-02-01 15:05:53 -0500

    add: SimpleBarChart component and update PieChart with Legend

--- frontend/src/components/charts/index.js
+++ frontend/src/components/charts/index.js
@@ -1,4 +1,5 @@
 export * from "@/components/charts/statistics-chart";
 export * from "@/components/charts/customPieChart.jsx";
 export * from "@/components/charts/simplePieChart.jsx";
-export * from "@/components/charts/custombarChart.jsx";
+export * from "@/components/charts/customBarChart.jsx";
+export * from "@/components/charts/simpleBarChart.jsx";
