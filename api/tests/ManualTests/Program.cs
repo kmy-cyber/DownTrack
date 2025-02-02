f2efc45dbe3679ab13a9c0fad703ea8c26431d31
JabelResendiz <jabelresendiz18@gmail.com>
2025-02-01 21:47:01 -0500

    tests ok

--- api/tests/ManualTests/Program.cs
+++ api/tests/ManualTests/Program.cs
@@ -2,7 +2,7 @@
 
 var apiClient = new ApiClient();
 
-// await User.RegisterUserAsync();
+await User.RegisterUserAsync();
 // await Section.RegisterSectionAsync();
 await Department.RegisterDepartmentAsync();
 // await Equipment.RegisterEquipmentAsync();
